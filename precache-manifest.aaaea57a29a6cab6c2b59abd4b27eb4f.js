683a176ddab72d83ca4c50d84c2afafecf3d0f5d
Your Name <you@example.com>
2019-11-18 17:25:58 +0100

    Updates

---
+++ precache-manifest.aaaea57a29a6cab6c2b59abd4b27eb4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0498012562012d271fcb17b8b095f6",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "afc045edfd3c3a3c8216",
+    "url": "/MovieApp/static/css/main.4f01ef2c.chunk.css"
+  },
+  {
+    "revision": "d589626b3afdb0e309bb",
+    "url": "/MovieApp/static/js/2.cb9be8c1.chunk.js"
+  },
+  {
+    "revision": "afc045edfd3c3a3c8216",
+    "url": "/MovieApp/static/js/main.eacf120d.chunk.js"
+  },
+  {
+    "revision": "bde5a214d5c86a0be95e",
+    "url": "/MovieApp/static/js/runtime-main.e91d9d72.js"
+  }
+]);
